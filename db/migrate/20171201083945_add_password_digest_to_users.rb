93cbe4f80c78d290c9501b4a78fbad4363242dfa
Ivan <ivan@Ivans-MacBook-Pro.local>
2017-12-01 14:37:42 +0400

    Fix migration

--- db/migrate/20171201083945_add_password_digest_to_users.rb
+++ db/migrate/20171201083945_add_password_digest_to_users.rb
@@ -1,5 +1,8 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :password_digest, :string
+    if Rails.env == "production"
+    else
+      add_column :users, :password_digest, :string
+    end
   end
 end
