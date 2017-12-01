0bf20c6e30d282c9084a03395ee477000952a6ac
Ivan <ivan@Ivans-MacBook-Pro.local>
2017-12-01 14:55:11 +0400

    Add minitest for heroku

--- db/migrate/20171201083945_add_password_digest_to_users.rb
+++ db/migrate/20171201083945_add_password_digest_to_users.rb
@@ -1,8 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.1]
   def change
-    if Rails.env == "production"
-    else
-      add_column :users, :password_digest, :string
-    end
+    add_column :users, :password_digest, :string
   end
 end
